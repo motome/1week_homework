9d9673835f2b8ff38038c068a54dd8cc0ad7e2ae
motome <sigmapinefield15@gmail.com>
2023-09-25 17:25:20 +0900

    first commit

---
+++ class_review/c-1.py
@@ -0,0 +1,13 @@
+class Customer:
+    def __init__(self, first_name, family_name):
+        self.first_name = first_name
+        self.family_name = family_name
+
+    def full_name(self):
+        return f"{self.first_name} {self.family_name}"
+
+ken = Customer(first_name="Ken", family_name="Tanaka")
+print(ken.full_name())  # "Ken Tanaka"
+
+tom = Customer(first_name="Tom", family_name="Ford")
+print(tom.full_name())  # "Tom Ford"
